38bbe23c68a2334c0494db1d2f2b2b3d2d514059
Andrey Semenov <semenov.orc@gmail.com>
2024-09-27 18:19:03 +0300

    add bootstrap into index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 import './index.css'
+import AppLayout from './pages/AppLayout'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <AppLayout />,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
