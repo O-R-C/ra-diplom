dac76667a1ea36b0499e7665348296e15ac7f01f
Andrey Semenov <semenov.orc@gmail.com>
2024-09-27 23:19:50 +0300

    add HomeSearchForm

--- src/features/home/HomeSearchForm.jsx
+++ src/features/home/HomeSearchForm.jsx
@@ -1,21 +1,61 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export default function HomeSearchForm() {
+  const navigate = useNavigate()
+  const [isHidden, setIsHidden] = useState(true)
+  const [searchValue, setSearchValue] = useState('')
+
+  function toCatalog() {
+    const url = '/catalog?search=' + searchValue
+
+    setSearchValue('')
+    navigate(url)
+  }
+
+  const handleClick = () => {
+    if (!searchValue?.trim()) {
+      setIsHidden((isHidden) => !isHidden)
+      return
+    }
+
+    toCatalog()
+  }
+
+  const handleChange = (event) => {
+    setSearchValue(event.target.value)
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    handleClick()
+  }
+
   return (
     <>
-      <div
+      <label
+        htmlFor='search-expander-input'
         data-id='search-expander'
         className='header-controls-pic header-controls-search'
-      ></div>
+        onClick={handleClick}
+      ></label>
 
-      <form
-        data-id='search-form'
-        className='header-controls-search-form form-inline invisible'
-      >
-        <input
-          className='form-control'
-          placeholder='Поиск'
-          type='search'
-        />
-      </form>
+      {!isHidden && (
+        <form
+          onSubmit={handleSubmit}
+          data-id='search-form'
+          className={`header-controls-search-form `}
+        >
+          <input
+            className='form-control pr-5'
+            placeholder='Поиск'
+            type='search'
+            onChange={handleChange}
+            value={searchValue}
+            id='search-expander-input'
+          />
+        </form>
+      )}
     </>
   )
 }
