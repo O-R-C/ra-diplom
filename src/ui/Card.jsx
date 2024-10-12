4734fb8b058e9a78c4b1154c4ed1726f0f6cd839
Andrey Semenov <semenov.orc@gmail.com>
2024-10-12 10:53:40 +0300

    add styles for card align

--- src/ui/Card.jsx
+++ src/ui/Card.jsx
@@ -1,38 +1,22 @@
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
-import PreLoader from './PreLoader'
-import { useState } from 'react'
 
 export default function Card({ item: { images, title, price, id } }) {
-  const [isSelected, setIsSelected] = useState(false)
-
-  const handleClick = () => {
-    setIsSelected(!isSelected)
-  }
-
-  if (isSelected && navigation.state === 'loading')
-    return (
-      <div className='col-4 d-flex justify-content-center align-items-center'>
-        <PreLoader />
-      </div>
-    )
-
   return (
     <div className='col-4'>
-      <div className='card'>
+      <div className='card h-100'>
         <img
           src={images[0]}
           className='card-img-top img-fluid'
           style={{ height: '250px', objectFit: 'contain' }}
           alt={title}
         />
-        <div className='card-body d-flex flex-column'>
+        <div className='card-body d-flex flex-column justify-content-end'>
           <p className='card-text'>{title}</p>
-          <p className='card-text mt-auto'>{price} руб.</p>
+          <p className='card-text'>{price} руб.</p>
           <Link
             to={`/catalog/${id}`}
             className='btn btn-outline-primary'
-            onClick={handleClick}
           >
             Заказать
           </Link>
